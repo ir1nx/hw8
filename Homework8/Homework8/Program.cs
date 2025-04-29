f79627b8c79689c4fd4909889fc2af87424292bd
ir1nx <nikita058526@gmail.com>
2025-04-29 17:42:25 +0300

    Task1

--- Homework8/Homework8/Program.cs
+++ Homework8/Homework8/Program.cs
@@ -1,10 +1,36 @@
 ﻿namespace Homework8
 {
+    public class AgileLinkedList<T>
+    {
+        public Node<T> First {  get; set; }
+        public Node<T> Last { get; set; }
+        int Count { get; set; }
+        public class Node<T>
+        {
+            public T Data { get; set; }
+            public Node<T> Next { get; set; }
+            public Node<T> Prev { get; set; }
+            public Node(T data, Node<T> prev, Node<T> next)
+            {
+                Data = data;
+                Prev = prev;
+                Next = next;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"first = {First.Data} last = {Last.Data} count = {Count}";
+        }
+
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
